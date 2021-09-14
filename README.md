0115cf767bdaca7726ea79425caf6337b026c8be
Renato Magrini <renatomagrini@gmail.com>
2021-09-15 00:12:05 +0200

    [1.1] [INITIALIZING] Created Spring Boot with AWS Dependency and AmazonConfig Class

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# file-storage
-File Storage Application using Spring Boot, React and AWS S3
+# File Storage Application using Spring Boot, React and AWS S3
+
+
+
+## Development Process:
+
+
+### STORY 1: [INITIALIZING]
+TASKS:
+- [1.1] Create Spring Boot Project, Java AWS SDK dependency and AmazonConfig
+- [1.2] 
---
+++ file-storage-server/src/main/java/com/rvmagrini/filestorageserver/FileStorageServerApplication.java
@@ -0,0 +1,13 @@
+package com.rvmagrini.filestorageserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStorageServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStorageServerApplication.class, args);
+	}
+
+}
